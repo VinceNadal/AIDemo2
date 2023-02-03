8e2c0447175f2b5aeb35a8aeeec76fe711a5f1c1
Christian Benedict Gonzales <chgonzales@fullscale.io>
2023-02-04 00:31:18 +0800

    Create Frontend w ContactForm component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ContactForm from "./Components/ContactForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contacts Management System</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
+
+// 2 Primary React Components
+// ContactForm Component
+// ContactList Component
